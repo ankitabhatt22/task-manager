e30d6c68c290325e7c41229001223a4b00adf591
Ankita Bhatt <ankitabhatt528@gmail.com>
2026-06-06 17:18:44 +0530

    Implement create task endpoint

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+let tasks = [];
+
 router.get("/", (req, res) => {
-  res.json([]);
+  res.json(tasks);
+});
+
+router.post("/", (req, res) => {
+
+  const task = {
+    id: Date.now(),
+    title: req.body.title,
+    description: req.body.description || "",
+    dueDate: req.body.dueDate || null,
+    completed: false,
+    createdAt: new Date()
+  };
+
+  tasks.unshift(task);
+
+  res.status(201).json(task);
 });
 
 module.exports = router;
